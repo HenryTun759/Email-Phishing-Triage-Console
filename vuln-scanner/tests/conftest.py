d7871407b3106e664e6a364317aace91ae3dffec
HenryTun759 <zzayarlinhtun08@gmail.com>
2026-09-05 23:24:17 +1000

    Ensure CI test settings are deterministic

--- vuln-scanner/tests/conftest.py
+++ vuln-scanner/tests/conftest.py
@@ -1,9 +1,9 @@
 import os
 
-os.environ.setdefault("JWT_SECRET", "test-secret-that-is-at-least-32-characters-long")
-os.environ.setdefault("ADMIN_PASSWORD", "Test-Admin-Password-123!")
-os.environ.setdefault("APP_ENV", "test")
-os.environ.setdefault("DATABASE_URL", "sqlite:///./test_suite.db")
+os.environ["JWT_SECRET"] = "test-secret-that-is-at-least-32-characters-long"
+os.environ["ADMIN_PASSWORD"] = "Test-Admin-Password-123!"
+os.environ["APP_ENV"] = "test"
+os.environ["DATABASE_URL"] = "sqlite:///./test_suite.db"
 
 from fastapi.testclient import TestClient
 import pytest
